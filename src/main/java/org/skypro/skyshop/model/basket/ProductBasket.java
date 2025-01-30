bab854811c2f565f8b15e9cd3e1fb3a65b36f2d6
imeli1987 <imeli1987@gmail.com>
2025-01-30 16:13:16 +0300

    Lifecycle of Spring Boot Application components

--- src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -1,12 +1,14 @@
 package org.skypro.skyshop.model.basket;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@SessionScope
 @Service
 public class ProductBasket{
     private final Map<UUID, Integer> productBasket;
